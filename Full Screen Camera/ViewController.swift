25ee0202134669c320ffff315e0ab5d6baecacbe
Darren Jones <1425808+ddaddy@users.noreply.github.com>
2024-07-20 16:20:59 +0200

    Initial Commit

---
+++ Full Screen Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Full Screen Camera
+//
+//  Created by Darren Jones on 20/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
